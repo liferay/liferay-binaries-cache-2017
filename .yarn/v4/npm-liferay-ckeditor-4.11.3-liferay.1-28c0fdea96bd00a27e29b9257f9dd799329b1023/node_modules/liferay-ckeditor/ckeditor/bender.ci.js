e9b57c5f55d923c316d0770cfe2846efe39785f8
Peter Shin <peter.shin@liferay.com>
2019-04-30 22:24:52 -0700

    Revert "Commit cache (Yarn)"
    This reverts commit 641dcbf8dcc2f5bbb74c8a8daf15cc5959686e38.

--- .yarn/v4/npm-liferay-ckeditor-4.11.3-liferay.1-28c0fdea96bd00a27e29b9257f9dd799329b1023/node_modules/liferay-ckeditor/ckeditor/bender.ci.js
+++
@@ -1,1 +0,0 @@
-﻿var config=require("./bender");config.startBrowser=process.env.BROWSER||"Chrome";config.isTravis=!0;config.startBrowserOptions={Chrome:"--headless --disable-gpu",Firefox:"-headless"};module.exports=config;
--- .yarn/v4/npm-senna-2.7.7-6bb6caba81335cb36e3f605fb4bc64369dca6505/node_modules/senna/src/app/version.js
+++
@@ -1,7 +0,0 @@
-/**
- * @returns String containing the current senna version
- */
-
-const version = '<%= version %>';
-
-export default version;
